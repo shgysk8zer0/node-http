3804674419ff2cd33bd5bbc6b436c4c8e87e8bec
Chris Zuber <shgysk8zer0@gmail.com>
2023-09-22 10:05:54 -0700

    Remove test script

--- test.js
+++
@@ -1,8 +0,0 @@
-import './polyfills/all.js';
-import { parseMultipartFormData } from './form-data.js';
-import { req } from './req.js';
-
-console.log(
-	await parseMultipartFormData(req)
-		.then(data => Object.fromEntries(data.entries()))
-);
