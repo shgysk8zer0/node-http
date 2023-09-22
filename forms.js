dea795dbeb23ff689a7a6328eb2cafc79e3708bc
Chris Zuber <shgysk8zer0@gmail.com>
2023-09-22 10:00:38 -0700

    Fix parsing of forms

--- forms.js
+++ forms.js
@@ -1,5 +1,7 @@
 import { FORM_MULTIPART, FORM_URL_ENCODED } from './types.js';
 
+const PATTERN = /^(\r\n)*(?:Content-Disposition:\s*form-data;\s*name="(?<name>[^"]*)";?)(?:\s*filename="(?<filename>[^"]*)")?;?(?:\r\nContent-Type:\s*(?<contentType>[^\r\n]+))?;?(\r\n){2}(?<data>[^]*)?(\r\n)$/i;
+
 /**
  * Parse multipart/form-data and create a FormData object from a Response object.
  * @param {Request|Response} response - The Response object containing headers and body.
@@ -7,10 +9,6 @@ import { FORM_MULTIPART, FORM_URL_ENCODED } from './types.js';
  * @returns {Promise<FormData>} - A promise that resolves to a FormData object.
  */
 
-// @TODO Validate boundary
-// @TODO Make case-insensitive
-// @TODO Check for ReDoS vulnerabilities
-// @TODO Improve matching for data
 export async function parseMultipartFormData(response) {
 	const contentType = response.headers.get('Content-Type');
 	const body = await response.text();
@@ -20,23 +18,18 @@ export async function parseMultipartFormData(response) {
 	}
 
 	const boundary = contentType.split(';')[1].trim().split('=')[1];
-	const regexPattern = new RegExp(
-		// `--${boundary}\\r?\\n(?:Content-Disposition:\\s*form-data;\\s*name="(?<name>[^"]+)"(?:;\\s*filename="(?<filename>[^"]+)")?(\\r?\\n)?(?:Content-Type:\\s*(?<contentType>[^\\r\\n]+))?);?(\\r?\\n){2}(?<data>.+)`, 'gi'
-		`--${boundary}\\r?\\n(?:Content-Disposition:\\s*form-data;\\s*name="(?<name>[^"]+)"(?:;\\s*filename="(?<filename>[^"]+)")?(\\r?\\n)?(?:Content-Type:\\s*(?<contentType>[^\\r\\n]+))?);?(\\r?\\n){2}(?<data>[^]*)(?=\\r?\\n?--${boundary}-?-?)`, 'gi'
-		// (?:(?!\\r?\\n--${boundary})[^])*(?<data>)
-		// (?s:(?!\\n?--${data}$)(?<data>.*))
-	);
-
 	const formData = new FormData();
-	for (let match; (match = regexPattern.exec(body)); ) {
-		// console.log(match);
-		const { name, filename, contentType, data = '' } = match?.groups ?? {};
-		console.log({ [name]: data });
 
-		if (typeof filename === 'string') {
-			formData.append(name, new File([data], filename, { type: contentType ?? 'application/octet-stream' }));
-		} else {
-			formData.append(name, data);
+	for (const part of body.split(`--${boundary}`)) {
+		// End of body will be `--${boundary}--`, so final part will be "--" with newlines.
+		if  (part.trim().length > 2) {
+			const { name, filename, contentType, data = '' } = PATTERN.exec(part)?.groups ?? {};
+
+			if (typeof filename === 'string') {
+				formData.append(name, new File([data], filename, { type: contentType ?? 'application/octet-stream' }));
+			} else {
+				formData.append(name, data);
+			}
 		}
 	}
 
@@ -54,16 +47,15 @@ export async function parseUrlEncodedFormData(response) {
 	const body = await response.text();
 
 	if (typeof contentType !== 'string' || ! contentType.startsWith(FORM_URL_ENCODED)) {
-		throw new Error('Invalid Content-Type, expected application/x-www-form-urlencoded');
+		throw new Error(`Invalid Content-Type, expected ${FORM_URL_ENCODED}`);
 	}
 
-	const formData = new URLSearchParams(body);
+	const params = new URLSearchParams(body);
+	const data = new FormData();
 
-	// Convert URLSearchParams to a plain object
-	const formDataObject = {};
-	for (const [key, value] of formData) {
-		formDataObject[key] = value;
+	for (const [key, value] of params) {
+		data.append(key, value);
 	}
 
-	return formDataObject;
+	return data;
 }
